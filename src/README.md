b2dcf002ae28d375e3323986b71eca58d3e7e47b
Jose Canahui <jose.canahui@gmail.com>
2017-12-27 20:35:22 -0500

    First commit, boilerplate code for the tracker app

---
+++ src/README.md
@@ -0,0 +1,9 @@
+# GeoSensor Tracker
+
+This project uses MapBox to display sensor data over time. It is a static website build with ReactJS and Redux.
+
+## Run
+
+To install the packages run `npm install`.
+
+Next, run `webpack`, and check out the index file in the newly created dist folder.
---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {render} from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
+
+require('./app.scss');
+
+import Home from 'components/LogIn';
+
+render((
+  <Router>
+    <Route path="/" component={Home}>
+    </Route>
+  </Router>
+), document.getElementById('root'));
